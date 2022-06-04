6da32c67f251c4cc64c313b454a7046ab3b77b98
Valentine Riedman <underblueskysismile@gmail.com>
2022-06-04 16:46:28 -0500

    pics added to server module

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/solid_gold_date.jpg', description: 'Tasha and I when we first started dating!', likes: 0 },
+    { id: 2, path: 'images/wedding_day.jpg', description: 'Tasha and I when we got married. So happy already!', likes: 0 },
+    { id: 3, path: 'images/aquasox_game.jpg', description: 'We loved going to the Evertt AquaSox games in Washington State.', likes: 0 },
+    { id: 4, path: 'images/portland_friends.jpg', description: 'One of my besties and I enjoying our trip to Portland.', likes: 0 },
+    { id: 5, path: 'images/waffles.jpg', description: 'Waffles the day we saw Tater Tot for the first time. She needed ice cream.', likes: 0 },
+    { id: 6, path: 'images/tatertot.jpg', description: 'Tater Tot took some time warming up to humans. This is his first day with us.', likes: 0 },
+
 ];
 
 module.exports = galleryItems;
