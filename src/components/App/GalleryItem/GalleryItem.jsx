727445d678e640964f4e7f98816e00395352c667
Valentine Riedman <underblueskysismile@gmail.com>
2022-06-07 15:26:27 -0500

    put not working quite right not sure what i am missing

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import './GalleryItem.css';
 
@@ -10,6 +11,12 @@ function GalleryItem( props ){
 
     const handleClick = ()=>{
         setClicks( clicks +1 );
+        axios.put('/gallery/like/:id').then( ( response )=>{
+            console.log( response.data );
+        }).catch( ( err ) =>{
+            console.log( err );
+            alert( 'error getting likes' );
+        })
     }
 
 
@@ -21,13 +28,14 @@ function GalleryItem( props ){
         <div onClick= { toggleShow } className='photo'>
             {
                 show?
-                <h2><img class="description"src= {props.photo.path}/> {props.photo.description} 
+                <h2><img className="description"src= {props.photo.path}/> {props.photo.description} 
                 </h2>
                 :
                 <img src= {props.photo.path}/>
             }
             <h5><button onClick={ handleClick }>Like</button>Likes:{ clicks }</h5>
         </div>
+        
     );
 };
 
